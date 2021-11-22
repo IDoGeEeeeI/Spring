3fddbdbbf3ed8fb953e7a7c7606b411dce0846ed
IDoGeEeeeI <79867275+IDoGeEeeeI@users.noreply.github.com>
2021-11-22 18:44:58 +0300

    task4

---
+++ src/main/java/com/geekbrains/spring/web/controllers/ProductController.java
@@ -0,0 +1,31 @@
+package com.geekbrains.spring.web.controllers;
+
+import com.geekbrains.spring.web.data.Product;
+import com.geekbrains.spring.web.services.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/products/delete/{id}")
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+
+    @GetMapping("/products/change_cost")
+    public void changeCost(@RequestParam Long productId, @RequestParam Integer delta) {
+        productService.changeCost(productId, delta);
+    }
+}
